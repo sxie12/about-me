bd9555960f78a0fea1e78c138fb6104461a102ad
Shunping Xie <sxie12@illinois.edu>
2015-09-05 19:17:12 -0500

    updated

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 // This is the JavaScript file
 
 $(window).scroll(function() {
-    /* Use math expression to make it fade depending on height
-     * and limit the amount of fade to 0.6
-     */
     var topOff = $(window).scrollTop();
-    if (topOff <= 0) {
-        $('.desktop_header_table').css("opacity", 1);
-        $('.header').css("opacity", 1);
-    } 
-    else {
-        $('.desktop_header_table').css("opacity", 0.6);
-        $('.header').css("opacity", 0.6);
-    }
+    var opacityLevel = Math.max(0.6, 1 - topOff/800);
+    $('.desktop_header_table').css("opacity", opacityLevel);
+    $('.header').css("opacity", opacityLevel);
 });
 
 var hover = $('.header').hover(function() {
-        $('.desktop_header_table').css("opacity", 1);
-        $('.header').css("opacity", 1);
+    $('.desktop_header_table').css("opacity", 1);
+    $('.header').css("opacity", 1);
 });
 
 
